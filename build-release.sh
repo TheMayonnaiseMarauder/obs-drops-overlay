e5f715b7afec999701b3f9958e7b47791c627c2e
TheMayonnaiseMarauder <168603666+TheMayonnaiseMarauder@users.noreply.github.com>
2024-05-14 21:38:24 -0400

    build script

---
+++ build-release.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+if [ ! -d "build" ]; then
+  mkdir build
+fi
+pv="0.0.1"
+pn="obs-drops-overlay"
+GOARCH=amd64
+declare -a goos=("windows" "linux" "darwin")
+for i in "${goos[@]}"; do
+  GOOS="${i}"
+  if [ "${i}" == "windows" ]; then
+    CGO_ENABLED=0 go build -v -a -ldflags="-s -w" -o "build/${pn}-${pv}-${i}-${GOARCH}.exe" .
+  else
+    CGO_ENABLED=0 go build -v -a -ldflags="-s -w" -o "build/${pn}-${pv}-${i}-${GOARCH}" .
+  fi
+done
+for f in build/*; do
+  file "${f}"
+done
+
